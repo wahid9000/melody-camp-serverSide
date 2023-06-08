b8397d5fd33ff7c209e1c4ef6710243a53b459a2
Wahid Ahmed Chowdhury <wahidchowdhury9000@gmail.com>
2023-06-09 01:17:50 +0600

    API created to make change users role to admin or instructor in client side

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ app.get("/", (req, res) => {
   res.send("Melody Server is Running");
 });
 
-
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wy87kp4.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wy87kp4.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,69 +28,105 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const classesCollection = client.db('melodyDB').collection('classesCollection');
-    const instructorsCollection = client.db('melodyDB').collection('instructorsCollection');
-    const mySelectedClassCollection = client.db('melodyDB').collection('selectedClassCollection');
-    const allUsersCollection = client.db('melodyDB').collection('allUsersCollection');
+    const classesCollection = client
+      .db("melodyDB")
+      .collection("classesCollection");
+    const instructorsCollection = client
+      .db("melodyDB")
+      .collection("instructorsCollection");
+    const mySelectedClassCollection = client
+      .db("melodyDB")
+      .collection("selectedClassCollection");
+    const allUsersCollection = client
+      .db("melodyDB")
+      .collection("allUsersCollection");
 
     //users related API's
-    app.post('/users', async(req, res) => {
+
+    app.get("/users", async (req, res) => {
+      const result = await allUsersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
       const saveUser = req.body;
 
-      const query = {email: saveUser.email}
+      const query = { email: saveUser.email };
       const existingUser = await allUsersCollection.findOne(query);
       console.log(existingUser);
-      if(existingUser){
-        return res.send({message: "User already exists"})
+      if (existingUser) {
+        return res.send({ message: "User already exists" });
       }
 
       const result = await allUsersCollection.insertOne(saveUser);
       res.send(result);
-    })
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await allUsersCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+      const result = await allUsersCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
 
 
-    
+
+
     //mySelectedClass related API
-    app.get('/mySelectedClass', async(req, res) => {
-        const email = req.query.email;
-        if(!email){
-            res.send([])
-        }
-        const query = {email: email}
-        const result = await mySelectedClassCollection.find(query).toArray();
-        res.send(result);
-    })
+    app.get("/mySelectedClass", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await mySelectedClassCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    app.post('/mySelectedClass', async(req, res) => {
-        const selectedClass = req.body;
-        const result = await mySelectedClassCollection.insertOne(selectedClass)
-        res.send(result)
-    })
+    app.post("/mySelectedClass", async (req, res) => {
+      const selectedClass = req.body;
+      const result = await mySelectedClassCollection.insertOne(selectedClass);
+      res.send(result);
+    });
 
-    app.delete('/mySelectedClass/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await mySelectedClassCollection.deleteOne(query);
-        res.send(result);
-    })
-    
+    app.delete("/mySelectedClass/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await mySelectedClassCollection.deleteOne(query);
+      res.send(result);
+    });
 
-
-    
     //classes related API
-    app.get('/classes', async(req, res) => {
-        const result = await classesCollection.find().sort({ students_number: -1 }).toArray();
-        res.send(result)
-    })
-
-
-
+    app.get("/classes", async (req, res) => {
+      const result = await classesCollection
+        .find()
+        .sort({ students_number: -1 })
+        .toArray();
+      res.send(result);
+    });
 
     //instructors related API
-    app.get('/instructors', async(req, res) => {
-        const result = await instructorsCollection.find().toArray();
-        res.send(result);
-    })
+    app.get("/instructors", async (req, res) => {
+      const result = await instructorsCollection.find().toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
