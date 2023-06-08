b183215879567a6ed2b50e5b58d127134f35644f
Wahid Ahmed Chowdhury <wahidchowdhury9000@gmail.com>
2023-06-09 01:26:30 +0600

    jwt installed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
